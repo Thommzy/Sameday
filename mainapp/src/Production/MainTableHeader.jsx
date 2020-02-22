26f8347c7d8a62afa50dbf5ae3654fd76181b596
Thommzy <tomiwaobeisun@yahoo.com>
2020-02-22 09:20:01 +0100

    production Title header Line almost done

--- mainapp/src/Production/MainTableHeader.jsx
+++ mainapp/src/Production/MainTableHeader.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+import { Menu, Button } from "semantic-ui-react";
+import LabelComp from "../Components/LabelComp";
 
 class MainTableHeader extends Component {
   render() {
-    return <div>MainTableHeader</div>;
+    return (
+      <div className="MainTableHeaderClass">
+        <Menu secondary>
+          <div className="allProductionClass">
+            All Production 192
+            <div className="divide"></div>
+          </div>
+
+          <div className="awaitingProductionClass">
+            Awaiting Production <LabelComp numberText="19" color="red" />
+            <div className="divide"></div>
+          </div>
+          <div>Completed</div>
+          <Menu.Menu position="right">
+            <div className="overallRightProdduction">
+              <Button>Print Worksheet</Button>
+            </div>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    );
   }
 }
 
